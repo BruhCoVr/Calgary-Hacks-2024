a59f4f6b32cd4bbb901367fcdf5fade0c501c825
BruhCoVr <vgrin803@mtroyal.ca>
2024-02-18 02:50:04 -0700

    cafeteria map

---
+++ maps/cafeteria.gd
@@ -0,0 +1,50 @@
+extends Area2D
+
+
+var fireball_scene = preload("res://spells/fireball.tscn")
+var magicmissle_scene = preload("res://spells/magicmissle.tscn")
+var lightning_scene = preload("res://spells/lightning.tscn")
+var earthaoe_scene = preload("res://spells/earthaoe.tscn")
+var ice_scene = preload("res://spells/ice.tscn")
+var stun_scene = preload("res://spells/stun.tscn")
+
+
+func _on_player_ice(pos, direction):
+	var ice = ice_scene.instantiate()
+	ice.position = pos
+	ice.rotation_degrees = rad_to_deg(direction.angle()) +90
+	ice.direction = direction
+	add_child(ice)
+
+func _on_player_magicmissle(pos, direction):
+	var magicmissle = magicmissle_scene.instantiate()
+	magicmissle.position = pos
+	magicmissle.rotation_degrees = rad_to_deg(direction.angle())+90
+	magicmissle.direction = direction
+	add_child(magicmissle)
+
+func _on_player_fire(pos, direction):
+	var fireball = fireball_scene.instantiate()
+	fireball.position = pos
+	fireball.rotation_degrees = rad_to_deg(direction.angle())+90
+	fireball.direction = direction
+	add_child(fireball)
+
+func _on_player_earth():
+	var earthaoe = earthaoe_scene.instantiate()
+	earthaoe.position = $Player.position
+	add_child(earthaoe)
+
+func _on_player_lightning(pos, direction):
+	var lightning = lightning_scene.instantiate()
+	lightning.position = pos
+	lightning.rotation_degrees =rad_to_deg(direction.angle())+90
+	lightning.direction = direction
+	add_child(lightning)
+
+func _on_player_stun(pos, direction):
+	var stun = stun_scene.instantiate()
+	stun.position = pos
+	stun.rotation_degrees = rad_to_deg(direction.angle())+90
+
+	add_child(stun)
